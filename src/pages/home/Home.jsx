d9736032f95eb3f154670628543bc1cc0886aeda
Sanjay-Khadka <sanjay1khadka@gmail.com>
2023-02-13 20:25:50 +0545

    features added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,6 +15,51 @@ const Home = () => {
           <Category item={card} key={card.id} />
         ))}
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="left-section">
+            <h2>A whole world of freelance talent at your fingertips</h2>
+            <div className="title">
+              <img src="./img/check.svg" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.svg" alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your project within
+              minutes.
+            </p>
+            <div className="title">
+              <img src="./img/check.svg" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.
+            </p>
+            <div className="title">
+              <img src="./img/check.svg" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Questions? Our round-the-clock support team is available to help
+              anytime, anywhere.
+            </p>
+          </div>
+          <div className="right-section">
+            <video
+              src="https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/vmvv3czyk2ifedefkau7"
+              controls
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
