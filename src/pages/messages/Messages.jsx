48acd9a9b2c7edae47ee5428fd6fbfc055446b0e
Sanjay-Khadka <sanjay1khadka@gmail.com>
2023-02-14 20:00:27 +0545

    created messages page and added styles

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
-import "./Messages.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiMail } from "react-icons/fi";
+import "./Messages.scss";
 const Messages = () => {
+  const currentUser = {
+    id: 1,
+    username: "sanjay",
+    isSeller: true,
+  };
   return (
-    <div>Messages</div>
-  )
-}
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/2" className="link">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                adipisci.
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/2" className="link">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                adipisci.
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/2" className="link">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                adipisci.
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/2" className="link">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                adipisci.
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/2" className="link">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                adipisci.
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Messages
+export default Messages;
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
-import "./Orders.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiMail } from "react-icons/fi";
+import "./Orders.scss";
 const Orders = () => {
+  const currentUser = {
+    id: 1,
+    username: "sanjay",
+    isSeller: true,
+  };
   return (
-    <div>Orders</div>
-  )
-}
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src="https://images.pexels.com/photos/5044316/pexels-photo-5044316.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                alt=""
+              />
+            </td>
+            <td>Photo</td>
+            <td>12300</td>
+            <td>34</td>
+            <td>
+              <button>
+                <FiMail color="white" size={20} />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src="https://images.pexels.com/photos/5044316/pexels-photo-5044316.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                alt=""
+              />
+            </td>
+            <td>Photo</td>
+            <td>12300</td>
+            <td>34</td>
+            <td>
+              <button>
+                <FiMail color="white" size={20} />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src="https://images.pexels.com/photos/5044316/pexels-photo-5044316.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                alt=""
+              />
+            </td>
+            <td>Photo</td>
+            <td>12300</td>
+            <td>34</td>
+            <td>
+              <button>
+                <FiMail color="white" size={20} />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src="https://images.pexels.com/photos/5044316/pexels-photo-5044316.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                alt=""
+              />
+            </td>
+            <td>Photo</td>
+            <td>12300</td>
+            <td>34</td>
+            <td>
+              <button>
+                <FiMail color="white" size={20} />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src="https://images.pexels.com/photos/5044316/pexels-photo-5044316.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                alt=""
+              />
+            </td>
+            <td>Photo</td>
+            <td>12300</td>
+            <td>34</td>
+            <td>
+              <button>
+                <FiMail color="white" size={20} />
+              </button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
